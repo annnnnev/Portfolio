bae9124836f28aa7961ca625ae59124b7424b777
annnnnev <89993@glr.nl>
2023-09-04 21:17:09 +0200

    animations nav

--- Index.php
+++ Index.php
@@ -4,14 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <script src="js/script.js" defer></script>
 </head>
 <body>
     <?php include 'util/navbar.html';?>
-    <div class="inleiding">       
+    <div class="inleiding">   
+        <h1>Over mij</h1>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
         <img src="media/kanye.jpg" alt="" >
     </div>
-    <div class="contact">
+    <div class="contact" id="Contact">
         <h1>Contact</h1>
         <form action="post" id="form1">
             <p>Message</p>
