0110042f53912f86d5f6c720b666cd97da9bbf53
annnnnev <89993@glr.nl>
2023-09-08 10:06:21 +0200

    footer

--- Index.php
+++ Index.php
@@ -14,7 +14,7 @@
 <div display="none" id="hiddendiv" width="0" height="0"></div>
 
     <?php include 'util/navbar.html';?>
-    <div class="inleiding" >   
+    <div class="inleiding" id="inleiding">   
         <div>
         <h1>Over mij</h1>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
@@ -26,6 +26,7 @@
         </div>
         <img src="media/kanye.jpg" alt="" >
     </div>
+    
     <div class="contact" id="Contact">
         <h1>Contact</h1>
         <form action="post" id="form1" action=”mailto:089993@glr.nl” method=”POST” enctype=”text/plain” name=”EmailForm”>
@@ -37,5 +38,6 @@
             <input type="submit" value="Send" id="button">
         </form>
     </div>
+    
 </body>
 </html>
